6c2d42193207ea3c1139fe6a478731fea3f6e046
Digvijay Rathore <rathore.digvijay10@gmail.com>
2020-05-03 17:58:51 +0530

    get element value on page

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+ * File: script.js
+ * Project: textToSpeech
+ * File Created: Sunday, 3rd May 2020 5:00:35 pm
+ * Author: digvijay (rathore.digvijay10@gmail.com)
+ */
+
+
+const playButton = document.getElementById('play-button');
+const pauseButton = document.getElementById('pause-button');
+const stopButton = document.getElementById('stop-button');
+const textInput = document.getElementById('text');
+const speedInput = document.getElementById('speed');
+
