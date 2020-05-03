71f089441face5d57fb9f5278b6d312587c3ecb2
Digvijay Rathore <rathore.digvijay10@gmail.com>
2020-05-03 18:51:32 +0530

    working script

--- script.js
+++ script.js
@@ -12,12 +12,43 @@ const stopButton = document.getElementById('stop-button');
 const textInput = document.getElementById('text');
 const speedInput = document.getElementById('speed');
 
+let currentChar;
 
 playButton.addEventListener('click', () => {
     playText(textInput.value);
 });
-
 pauseButton.addEventListener('click', pauseText);
-
 stopButton.addEventListener('click', stopText);
+speedInput.addEventListener('input', () => {
+    stopText();
+    playText(utterance.text.substring(currentChar));
+})
 
+
+const utterance = new SpeechSynthesisUtterance(text);
+utterance.addEventListener('end', () => {
+    textInput.disabled = false;
+})
+utterance.addEventListener('boundary', e => {
+    currentChar = e.charIndex;
+})
+
+function playText(text) {
+    if(speechSynthesis.paused && speechSynthesis.speaking){
+        return speechSynthesis.resume();
+    }
+    if(speechSynthesis.speaking) return;
+    utterance.text = text;
+    utterance.rate = speedInput.value || 1;
+    textInput.disabled = true;
+    speechSynthesis.speak(utterance);
+}
+
+function pauseText() {
+    if (speechSynthesis.speaking) speechSynthesis.pause()
+}
+
+function stopText() {
+    speechSynthesis.resume();
+    speechSynthesis.cancel();
+}
